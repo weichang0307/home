cddc867c9d231475074a9a588a47e08a4588fd4f
weichang0307 <83111522+weichang0307@users.noreply.github.com>
2021-07-04 11:47:54 +0800

    Add files via upload

--- javascript/myscript.js
+++ javascript/myscript.js
@@ -9,6 +9,12 @@ let allcanvas={
         
     ]
 }
+let allthreejs={
+    games:[
+        {herf:'https://weichang0307.github.io/3Dzombies/',name:'3Dzombies'},
+        {herf:'https://weichang0307.github.io/wave-interference/',name:'wave-interference'}
+    ]
+}
 let divhome=document.getElementById('home')
 let divsearch=document.getElementById('search')
 
@@ -27,11 +33,26 @@ function canvashtml(element,base,div){
     }
     
 }
+function threejshtml(element,base,div){
+    let inner=base
+    inner+='<h2>&emsp;'+element+'</h2>'
+    for(let i of allthreejs[element]){
+        inner+='<h3>&emsp;&emsp;<a href='+i.herf+' target="_blank" id='+i.name+'>'+i.name+'</a><br></h3>'
+    }
+    if(div!=null){
+        div.innerHTML=inner
+    }else{
+        return inner
+    }
+
+}
 function inithomepage(){
     let inner=''
     inner+='<h1>Canvas</h1>'
     canvashtml('games',inner,divhome)
     canvashtml('tools',divhome.innerHTML,divhome)
+    divhome.innerHTML+='<h1>Three js</h1>'
+    threejshtml('games',divhome.innerHTML,divhome)
 
 }
 inithomepage()
@@ -49,12 +70,17 @@ let input01=document.getElementById('input01')
 form01.addEventListener('submit',(e)=>{
     let searched=''
     e.preventDefault()
-    let keyword=input01.value.trim().toLowerCase()
+    let keyword=input01.value.trim().toLowerCase()/*
     for(let i of Object.keys(allcanvas)){
         if(i.includes(keyword)){
             searched+=canvashtml(i,'',null)
         }
     }
+    for(let i of Object.keys(allthreejs)){
+        if(i.includes(keyword)){
+            searched+=canvashtml(i,'',null)
+        }
+    }*/
     if(searched===''){
         for(let y of Object.keys(allcanvas)){
             for(let i of allcanvas[y]){
@@ -63,9 +89,13 @@ form01.addEventListener('submit',(e)=>{
                 }
             }
         }
-        
-        
-        
+        for(let y of Object.keys(allthreejs)){
+            for(let i of allthreejs[y]){
+                if(i.name.includes(keyword)){
+                    searched+='<h3>games&emsp;&emsp;<a href='+i.herf+' target="_blank" id='+i.name+'>'+i.name+'</a><br></h3>'
+                }
+            }
+        }
     }
     if(searched===''||keyword===''){
         alert('nothing is found by keyword:'+keyword)
